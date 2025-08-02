5df7c90490f4d9e8848f43933a8966161d9011ab
qused310 <zestfulgibbon@163.com>
2025-08-03 02:29:35 +0800

    Site updated: 2025-08-03 02:29:33

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/08/02/hello-world/","2025/08/02/hi/","2025/08/02/你好/","2025/08/02/这是一篇博文/"];function toRandomPost(){
+var posts=["2025/08/02/hi/","2025/08/02/这是一篇博文/","2025/08/02/你好/","2025/08/02/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
